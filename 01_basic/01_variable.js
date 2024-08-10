36a44e820e1bc719c11de9299d9e03b4994ab45b
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-10 13:53:18 +0000

    day 1

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,25 @@
+const accountId=131313
+let accountemail="sunny@gmail.com"
+var accountpassword="76767676"
+accountcity="mumbai"
+
+
+//accountId=2 not allowed
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+ accountemail="sunny@gmail.com"
+ accountpassword="3131313"
+ accountcity="benguluru"
+let accountstate;
+
+console.log(accountId);
+console.table([ accountId,accountemail,accountpassword,accountcity,accountstate]) 
+
+
+
+
+
+
