fac7fb45001b722516540a0c89d181d78c015e39
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-12 12:51:20 +0000

    day 2

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" //treat all js code as a newer version
+
+//alert(3+3) using nodejs not a browser//
+
+console.log(3+3) //code readibility must be very high//
+
+console.log("sunny")
+
+let name="sunny"
+let age=17
+let isloggedIn=true
+let state=null
+
+// Number=> 2 to power 53
+// bigInt= for huge value or number
+//string=""
+//boolean=trur/false
+//null=stand alone value #empty 
+//undefined=> value not assigned ";"
+//symbol=> unique
+//object= to known someone real value.
+console.log(typeof "hello");//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
+
+
+
