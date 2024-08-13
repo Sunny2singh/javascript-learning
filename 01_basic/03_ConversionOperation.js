e94c08ab5d59b7d791e4b4d6ac96c5e27c623006
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-13 15:23:41 +0000

    day3

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+let score="72abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"72"=>72
+//"72abc"=>NaN
+// True=>1;false=>0
+
+let isLoggedIn="1"
+
+let booleanisLoggedIn= boolean(isLoggedIn);
+console.log(isLoggedIn);
+
+//  all valuse output will be
+
+// 1=>true; 0=>false;
+// ""=> false;
+//"sunny"=> true;
+
+
+let someNumber =33
+
+let stringNumber =string(someNumber);
+console.log(stringNumber);
+
+
