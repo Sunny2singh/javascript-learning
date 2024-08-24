5ffeb5047444ba851e39c2c539bfc1087359bbde
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-24 11:37:11 +0000

    day 4

---
+++ 01_basic/03_Conversion.js
@@ -0,0 +1,36 @@
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+let str1= "hello"
+let str2=" sunny"
+let str3=str1+str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log(1 + 1 + "2");
+//console.log("1" + 1 + 2);
+//console.log(1 + 2 + "2");
+
+
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2-2);
+//console.log(2**3);
+//console.log(2%2);
+
+//console.log(+true)
+//console.log(+"");
+
+let gameCounter=100;
+gameCounter++;
+//console.log(gameCounter);
+
+//read about prefix and postfix at mdn
+
+let x=9;
+let y= x++
+console.log(`x:${x} y:${y}`);
+
+//link to study https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
