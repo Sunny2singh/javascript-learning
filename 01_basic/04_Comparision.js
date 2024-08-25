cc820ba9a7e8b4764004e94cb449994ab80d4986
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-25 11:45:58 +0000

    comparision

---
+++ 01_basic/04_Comparision.js
@@ -0,0 +1,27 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 <= 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+//always try to use this comparision bcz code readibility is must
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0);
+//console.log(null >= 0);
+//console.log(null == 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+
+// try to avoid this type of comparision 
+
+//=== after using 3 equal the code will be strickly checked 
+
+
+
