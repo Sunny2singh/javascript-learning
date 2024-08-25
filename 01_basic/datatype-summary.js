c5d968be8bfaf16a1a2566ea82fed47fd7f86a2b
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-25 13:04:02 +0000

    datatype summary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,46 @@
+// *******************PRIMITIVE******************************
+
+//   7 Types : String,Number,Null,Boolean,Undefined,Symbol,BigInt
+
+const Score = 100
+const score = 100.33
+
+const IsloggedIn = true
+const outtemperature = null
+let userEmail; 
+
+const Id = symbol('123')
+ //const anotherId =symbol('123')
+
+ //console.log(Id === anotherId);
+ 
+//const bigNumber = 343443463463654n
+
+//***************** Non Primitive(Reference)*******************
+
+//  * Array,Object,Function
+
+//example of array
+
+let heros = ["shaktimaan","marvel","captainamerica"] 
+
+//example of Object
+let myObj= {
+             name:"Sunny",
+             age: "18",
+} 
+
+//example of Function
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof myFunction);
+
+
+
+
+ 
+
+//for more notes check comment https://www.youtube.com/watch?v=suMvZWjjKbo
