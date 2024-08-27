022db5efe2a10753ce7592dee8041c27edf5f417
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-27 12:36:36 +0000

    stack aur heap ki kahani

---
+++ 01_basic/05_memory.js
@@ -0,0 +1,23 @@
+// stack(Premitive) Heap(Non-Premitive)
+
+let myYoutubename = "Sunnysingh"
+
+let anothername = "myYoutubename"
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    useremail:"user@gmail.com",
+    upi:"user@ybl"
+
+}
+
+let userTwo = userOne
+
+userTwo.email="sunny@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
