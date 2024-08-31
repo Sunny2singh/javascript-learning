60e18c059a8b332607e6c7cdc225a833b83ac665
Sunny2singh <sunny.singh81698@gmail.com>
2024-08-31 16:34:48 +0000

    02_basic

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,52 @@
+//Array
+
+//const myArr = [0, 1, 2, 3, 4]
+
+const myHero =["Shaktimaan","ironman"]
+
+const MyArr = new Array(0, 1, 2, 3, 4)
+
+//console.log(MyArr[0]);
+
+//Arr Method
+
+ /* MyArr.push(6) 
+MyArr.push(7)       
+// most used element is .push it add element 
+MyArr.pop(7)
+// .pop removes last element */
+
+//MyArr.unshift(9) 
+//unshift is used for showing element in front or to show first
+//MyArr.shift()
+//console.log(MyArr)
+//console.log(MyArr.includes(9)); // To know the iclude value
+//console.log(MyArr.indexOf(9)); 
+
+//const newArr = MyArr.join()
+//console.log(MyArr);
+//console.log(typeof newArr);
+
+//slice,spice
+
+console.log("A" , MyArr);
+
+const myn1 = MyArr.slice(1,3) 
+
+console.log(myn1);
+console.log("B", MyArr);
+
+const myn2 = MyArr.splice(1,3) 
+console.log(myn2);
+console.log("c", MyArr);
+
+
+
+
+
+ 
+
+
+
+
+
