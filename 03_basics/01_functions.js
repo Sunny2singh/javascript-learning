ef2a1be60c7940aa3125622fd51bc258b31d8cb2
Sunny2singh <sunny.singh81698@gmail.com>
2024-09-05 10:27:11 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,40 @@ return `${username} Just logged in`
 }
 
 //console.log(LogginUserMessage("sunny"))
-console.log(LogginUserMessage())
+//console.log(LogginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// ... is also called as rest and another name is spread 
+console.log(calculateCartPrice(200 , 400, 600, 2000))
+
+// ** how to pass object in function
+
+const User = {
+    userName : ("Sunny"),
+    Price : 199
+}
+
+function handleObject(anyObject){
+    console.log(`userName is ${anyObject.userName} and price is ${anyObject.Price}`)
+}
+//handleObject(User)
+
+// Direct Object Without passing object
+
+handleObject({
+    userName:("sunny"),
+    Price:299
+})
+
+// Direct Array
+
+const newArray = [100 , 300 , 400, 200]
+
+function returnSecondValue(getArray){
+    return getArray[3]
+}
+//console.log(returnSecondValue(newArray));
+
+console.log(returnSecondValue([100, 300, 400, 200]));
