0b668b35e99c3c4f001e9c2e839216cc34127f58
Sunny2singh <sunny.singh81698@gmail.com>
2024-09-05 15:49:19 +0000

    scopes

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,24 @@
+//var c = 300
+let a =300
+if(true) {
+    let a = 10
+    const b = 20
+  console.log("INNER :", a);
+  
+}
+console.log(a);
+//console.log(b);
+//console.log(c);
+
+//+++++++++++++++ NESTED SCOPE++++++++++++++++++
+
+function one() {
+     const userName = "Sunny"
+function two(){
+    const website = "youtube"
+    console.log(userName);
+}
+//console.log(website);
+two()
+}
+one()
