2404cf5512ae7cf48212613fc021cf903e67e290
Sunny2singh <sunny.singh81698@gmail.com>
2024-09-06 11:47:13 +0000

    scope level and mini hoisting

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -1,24 +1,51 @@
 //var c = 300
-let a =300
+//let a =300
 if(true) {
     let a = 10
     const b = 20
-  console.log("INNER :", a);
+  //console.log("INNER :", a);
   
 }
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
 
 //+++++++++++++++ NESTED SCOPE++++++++++++++++++
 
-function one() {
-     const userName = "Sunny"
-function two(){
-    const website = "youtube"
-    console.log(userName);
-}
-//console.log(website);
-two()
+function one(){
+   const userName = "Sunny"
+
+   function two(){
+    const website = "Youtube"
+    //console.log(userName);
+   }
+   //console.log(website);
+   two()
 }
 one()
+
+if(true){
+  const userName = "Sunny"
+
+  if (userName === "Sunny") {
+    const website = "youtube" 
+    //console.log(  website +  userName);
+    
+  }
+  //console.log(website);
+ 
+}
+//console.log(userName);
+
+//++++++++++++++++++INTERESTING+++++++++++++++++++
+
+console.log(addone(19))
+function addone(num){
+  return num + 1
+}
+
+
+const addtwo = function (num){
+  return num + 2
+}
+console.log(addtwo(18))
