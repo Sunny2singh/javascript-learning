636f43161d9303933cddb8bfff19d0b5106b38a7
Sunny2singh <sunny.singh81698@gmail.com>
2024-09-14 13:00:39 +0000

    Control flow code

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,42 @@
+// for Number Value
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("Febuary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default case match ");
+//         break;
+// }
+
+// for String Value
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("Febuary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match ");
+        break;
+}
